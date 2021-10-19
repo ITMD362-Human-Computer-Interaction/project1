fe0c810b8d709e1fd474f2886153b4e300c42cff
Maria Garcia <mgarcia18@hawk.iit.edu>
2021-10-18 22:04:22 -0500

    starting js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ /* TODO
+ - Add javascript for age validation
+ - Add javascript for form validation
+ - Add javasript for terms and service validation
+ - Add javascript for confirmation text
+ 
+ */
+
+ alert("testing JS");
