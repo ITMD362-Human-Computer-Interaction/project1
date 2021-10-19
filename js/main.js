2434f3c40b24f03e1bd3edd632e97ee27f193b8f
Maria Garcia <mgarcia18@hawk.iit.edu>
2021-10-18 22:47:11 -0500

    form contents are checked to make sure input is present

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
  /* TODO
- - Add javascript for age validation
- - Add javascript for form validation
- - Add javasript for terms and service validation
  - Add javascript for confirmation text
  
  */
 
- alert("testing JS");
+console.log("hello")
+document.getElementById("submit").addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if(inputCheck() && checkBoxes()){
+    console.log("input is good")
+  }
+
+ });
+
+ function inputCheck(){
+  primaryName = document.getElementById("fname").value;
+  familyName = document.getElementById("lname").value;
+  city = document.getElementById("city").value;
+  country = document.getElementById("country").value;
+  phone = document.getElementById("phone").value;
+
+  if(!primaryName || !familyName || !city || !country || !phone){
+    alert("Please fill in all fields");
+    return false
+  }
+
+  console.log(`Primary Name: ${primaryName} \n
+  Family Name: ${familyName}\n
+  City: ${city} \n
+  Country: ${country} \n
+  Phone: ${phone} \n
+  `);
+  return true;
+}
+
+ function checkBoxes(){
+  if(!document.getElementById("age-verification").checked){
+    alert("Must be over 18 to participate");
+    return false;
+  }
+  else if(!document.getElementById("agree").checked){
+    alert("Must agree to the terms of service");
+    return false;
+  }
+  else{
+    return true;
+  }
+ }
